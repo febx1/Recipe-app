d44cf201d65bb6e13bdeaccd3c989858f970a5eb
febx <georgefebin97@gmail.com>
2024-02-03 22:31:01 +0530

    new changes

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import MaxWidthWrapper from "./components/MaxWidthWrapper";
+
+export default function Home() {
+  return (
+    <MaxWidthWrapper className="bg-red-500">
+      <div>Hello World</div>
+    </MaxWidthWrapper>
+  );
+}
